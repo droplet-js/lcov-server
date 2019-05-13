077f2d70707aefd2e7a46d74c234b45b61eb7f9a
v7lin <v7lin@qq.com>
2019-05-13 21:02:11 +0800

    lcov-server

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 ./wait-for-it.sh ${COVERAGE_DB_SERVERNAME}:${COVERAGE_DB_PORT} -- echo "coverage db is up";
 
-lcov-server --serve --db mongodb://${COVERAGE_DB_SERVERNAME}:${COVERAGE_DB_PORT}/${COVERAGE_DB_DATABASE}
+lcov-server --serve --db mongodb://${COVERAGE_DB_SERVERNAME}:${COVERAGE_DB_PORT}/${COVERAGE_DB_DATABASE} --parser ${COVERAGE_PARSER} --basePath ${COVERAGE_BASEPATH}
